c9fc2f56ef5a6fa6bbb34168ce8d28a16e12b250
Fabio Colella <fcole90@gmail.com>
2025-05-31 18:16:49 +0200

    Update default recrding type to float32

--- src/recorder/main.py
+++ src/recorder/main.py
@@ -20,7 +20,7 @@ def __change_dir_to_pkg_root() -> None:
 
 def record(duration: int, frequency: int = 44100, channels: int = 2):
     recording = cast(
-        numpy.ndarray[Any, numpy.dtype[numpy.float64]],
+        numpy.ndarray[Any, numpy.dtype[numpy.float32]],
         sounddevice.rec(
             int(duration * frequency), samplerate=frequency, channels=channels
         ),
